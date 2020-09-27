33df44c2038a6cd3e6214a005adddf8397e5d3fd
sandamaligeethika <sgeethika98@gmail.com>
2020-09-27 12:56:20 +0530

    Cafe Create-Retrieve

--- app/src/main/java/com/example/mad/CafeFirst.java
+++ app/src/main/java/com/example/mad/CafeFirst.java
@@ -4,21 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class CafeFirst extends AppCompatActivity {
+    EditText resName, addressLine1,addressLine2,city,contactNo,email;
+    Button btnContinue,btnSave;
+    Spinner spinner;
 
-    Button btnContinue;
+    DatabaseReference databaseCafe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafe_first);
 
-        Spinner spinner=(Spinner)findViewById(R.id.spinner);
+        databaseCafe = FirebaseDatabase.getInstance().getReference("cafes");
+
+        resName = findViewById(R.id.editTextResName);
+        addressLine1 = findViewById(R.id.editTextPostalAddress1);
+        addressLine2 = findViewById(R.id.editTextTextPostalAddress2);
+        city=findViewById(R.id.editTextPostalAddress3);
+        contactNo=findViewById(R.id.editTextPhone);
+        email=findViewById(R.id.editTextTextEmailAddress);
+
+        spinner=(Spinner)findViewById(R.id.spinner);
         ArrayAdapter<String> myAdapter= new ArrayAdapter<String>(CafeFirst.this,
                 android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.categories));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -29,9 +48,64 @@ public class CafeFirst extends AppCompatActivity {
         btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               Intent intent1 = new Intent(CafeFirst.this,CafeSecond.class);
-               startActivity(intent1);
+                Intent intent1 = new Intent(CafeFirst.this,Cafe_MyRestaurant.class);
+                startActivity(intent1);
             }
         });
+
+        btnSave=(Button)findViewById(R.id.buttonSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addCafe();
+            }
+        });
+    }
+
+    private void addCafe(){
+        String resname = resName.getText().toString().trim();
+        String addressline1 = addressLine1.getText().toString().trim();
+        String addressline2 = addressLine2.getText().toString().trim();
+        String addcity=city.getText().toString().trim();
+        String category = spinner.getSelectedItem().toString();
+        Integer phone = Integer.parseInt(contactNo.getText().toString());
+        String emailadd=email.getText().toString();
+
+        try{
+            if(TextUtils.isEmpty(resname)){
+                Toast.makeText(this,"Please add the name",Toast.LENGTH_SHORT).show();
+            }
+            else if(TextUtils.isEmpty(addressline1)){
+                Toast.makeText(this,"Please add the address line 1",Toast.LENGTH_SHORT).show();
+            }else if (TextUtils.isEmpty(addressline2)){
+                Toast.makeText(this,"Please add the address line 2",Toast.LENGTH_SHORT).show();
+            }else if (TextUtils.isEmpty(addcity)) {
+                Toast.makeText(this, "Please add the city name", Toast.LENGTH_SHORT).show();
+            }
+            else if (TextUtils.isEmpty(emailadd)){
+                Toast.makeText(this,"Please add the city name",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                String id = databaseCafe.push().getKey();
+
+                CafeRestaurant cafeRestaurant = new CafeRestaurant(id,resname,addressline1,addressline2,addcity,category,phone,emailadd);
+
+                databaseCafe.child(id).setValue(cafeRestaurant);
+
+                Toast.makeText(getApplicationContext(),"Added Successfully",Toast.LENGTH_SHORT).show();
+                clearControls();
+            }
+        }catch (NumberFormatException e){
+            Toast.makeText(getApplicationContext(),"Invalid contact number",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void clearControls(){
+        resName.setText("");
+        addressLine1.setText("");
+        addressLine2.setText("");
+        city.setText("");
+        contactNo.setText("");
+        email.setText("");
     }
 }
---
+++ app/src/main/java/com/example/mad/CafeList.java
@@ -0,0 +1,46 @@
+package com.example.mad;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class CafeList extends ArrayAdapter<CafeRestaurant>{
+    private Activity context;
+    private List<CafeRestaurant> cafeList;
+
+    public CafeList(Activity context, List<CafeRestaurant> cafeList){
+        super(context,R.layout.cafe_list,cafeList);
+        this.context=context;
+        this.cafeList=cafeList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.cafe_list,null,true);
+
+        TextView cafeName = (TextView)listViewItem.findViewById(R.id.textViewRestName);
+        TextView cafeLocation = (TextView)listViewItem.findViewById(R.id.textViewLocation);
+        TextView cafeCategory = (TextView)listViewItem.findViewById(R.id.textViewCategory);
+
+
+        CafeRestaurant cafeRestaurant = cafeList.get(position);
+
+        cafeName.setText(cafeRestaurant.getName());
+        cafeLocation.setText(cafeRestaurant.getAddressLine1());
+        cafeCategory.setText(cafeRestaurant.getCategory());
+
+
+        return listViewItem;
+    }
+
+}
